789808ca1c32028bcfdca66c5d2968619f4cb997
Jecel Assumpcao Jr <jecel@merlintec.com>
2023-12-18 18:22:23 -0300

    initial configuration and window test

--- SoftCoreCompare.py
+++ SoftCoreCompare.py
@@ -1,3 +1,20 @@
 #!/usr/bin/python3
 
-print("will compare soft cores")
+defaults = {'excludefpgas': [], 'excludeprojects': [], 'autogen': []}
+
+import json
+from pathlib import Path
+d = Path('defaults.json')
+if d.is_file():
+    with d.open() as j:
+        defaults = json.loads(j.read())
+print(defaults)
+
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+root.mainloop()
